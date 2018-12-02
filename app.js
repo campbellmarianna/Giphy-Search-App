bf5c26ac655c39ba91e52e899f0cd559938c553f
Marianna <mckd2018@gmail.com>
2018-12-02 15:01:51 -0800

    add answer to http method question

--- app.js
+++ app.js
@@ -38,51 +38,51 @@ app.get('/greetings/:name', function (req, res) {
     var name = req.params.name;
     res.render('greetings', {name: name});
 });
+
 // THIS
-// Question: Why is that what I submit in a input text field appears in the url path
-// app.get('/', function (req, res) {
-//     var queryString = req.query.term;
-//     // Encode the query string to remove white spaces and restricted characters
-//     var term = encodeURIComponent(queryString);
-//     // Put the search term into the giphy API search URL
-//     var url = 'http://api.giphy.com/v1/gifs/search?q=' + term + '&api_key=dc6zaTOxFJmzC'
-//
-//     http.get(url, function(response) {
-//         // Set encoding of response to utf8
-//         // READ ARTICLE ABOUT THIS TONIGHT
-//         response.setEncoding('utf8');
-//
-//         var body = '';
-//
-//         response.on('data', function(d) {
-//             // Continuously update stream with data from giphy
-//             body += d;
-//         });
-//
-//         response.on('end', function() {
-//             // when data is fully received parse into JSON
-//             var parsed = JSON.parse(body);
-//             // Render the home template and pass the gif data into the template
-//             res.render('home', {gifs: parsed.data})
-//         });
-//     });
-// })
+app.get('/', function (req, res) {
+    var queryString = req.query.term;
+    // Encode the query string to remove white spaces and restricted characters
+    var term = encodeURIComponent(queryString);
+    // Put the search term into the giphy API search URL
+    var url = 'http://api.giphy.com/v1/gifs/search?q=' + term + '&api_key=dc6zaTOxFJmzC'
+
+    http.get(url, function(response) {
+        // Set encoding of response to utf8
+        // READ ARTICLE ABOUT THIS TONIGHT
+        response.setEncoding('utf8');
+
+        var body = '';
+
+        response.on('data', function(d) {
+            // Continuously update stream with data from giphy
+            body += d;
+        });
+
+        response.on('end', function() {
+            // when data is fully received parse into JSON
+            var parsed = JSON.parse(body);
+            // Render the home template and pass the gif data into the template
+            res.render('home', {gifs: parsed.data})
+        });
+    });
+})
 
 // OR THAT
-app.get('/', function (req, response) {
-    // if you get a search term for gifs with that term
-    if (req.query.term) {
-        giphy.search(req.query.term, function (err, response) {
-            console.log(err);
-            response.render('home', {gifs: res.data});
-        });
-    // if you don't get a search term display trending gifs
-    } else {
-        giphy.trending(function (err, res) {
-            response.render('home', {gifs: res.data});
-        });
-    }
-});
+// app.get('/', function (req, response) {
+//     // if you get a search term for gifs with that term
+//     if (req.query.term) {
+//         giphy.search(req.query.term, function (err, response) {
+//             console.log(err);
+//             response.render('home', {gifs: res.data});
+//         });
+//     // if you don't get a search term display trending gifs
+//     } else {
+//         giphy.trending(function (err, res) {
+//             response.render('home', {gifs: res.data});
+//         });
+//     }
+// });
 
 
 // Web Server Check
